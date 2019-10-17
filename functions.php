81221dc6999922593068d4fae5fe974521adcf10
Riley Bathurst <riley@rileybathurst.com>
2019-10-18 12:57:36 +1300

    remove the double up

--- functions.php
+++ functions.php
@@ -617,7 +617,7 @@ function prefix_admin_discovery() {
 			wp_redirect( home_url() . '/sorry' );
 		} else {
 			// The Captcha is valid you can continue with the rest of your code
-			wp_redirect( home_url() . '/thanks' );
+			// wp_redirect( home_url() . '/thanks' );
 
 			// send the email before inserting into the database
 			//get form elements and email
@@ -770,103 +770,6 @@ function prefix_admin_discovery() {
 			// return safe if the inserted number is above zero and inserted to database
 			// the email may be sent even if the database doesn't update but better to false negative than false positive
 			if ($id>0)  {
-				//get form elements and email
-				$name = $_POST['name'];
-				$email = $_POST['email'];
-				$enq = $_POST['details'];
-
-				//email to riley@rileybathurst.com
-
-				// $to				= 'authenticalignmentwellness@gmail.com';
-				$developer		= 'riley@rileybathurst.com';
-				$subject		= 'Authentic Alignment Wellness Discovery Session for: '.$name;
-
-				//write email saved in the email.html files for reference I might be able to do something smarter pulling this in
-				$message = '
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml">
-		<head>
-			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-			<title>Authentic Alignment Wellness</title>
-		</head>
-
-		<body bgcolor="#ebebeb">
-
-			<table cellspacing="0" cellpadding="0" width="100%" bgcolor="#ebebeb" align="center">
-				<tbody>
-					<tr>
-						<table style="border-left: 2px solid #e6e6e6; border-right: 2px solid #e6e6e6;" cellspacing="0" cellpadding="25" width="605" align="center">
-
-							<tr>
-								<td width="596" align="center" style="background-color: #ffffff; border-top: 0px solid #000000; text-align: left; height: 50px;">
-									<p style="margin-bottom: 10px; font-size: 22px; font-weight: bold; color: #494a48; font-family: arial; line-height: 110%; text-align: center;">Authentic Alignment Wellness</p>
-								</td>
-							</tr>
-
-							<tr>
-								<td style="background-color: #ffffff; border-top: 0px solid #000000; text-align: left;" align="center">
-
-									<hr style="color:#d9d9d9;background-color:#d9d9d9;min-height:1px;border:none;"/>
-
-									<p>
-										Thanks for your contact, '.
-											$_POST['name'] .
-										' we will be in touch ASAP.
-									</p>
-
-									<hr style="color:#d9d9d9;background-color:#d9d9d9;min-height:1px;border:none;"/>
-
-									<p>
-										For your records the message was,
-									</p>
-
-									<p> ' .
-										$_POST['add_notes'] .
-									'</p>
-
-									<hr style="color:#d9d9d9;background-color:#d9d9d9;min-height:1px;border:none;"/>
-
-									<p>
-										We will contact you back on, ' .
-											$_POST['email'] .
-										' or '.
-											$_POST['phone'] .
-									'</p>
-
-									<hr style="color:#d9d9d9;background-color:#d9d9d9;min-height:1px;border:none;"/>
-
-								</td>
-							</tr>
-
-							<tr>
-								<td style="background-color: #ffffff; border-top: 0px solid #000000; text-align: center;" align="center">
-									<span style="font-size: 11px; color: #575757; line-height: 200%; font-family: arial; text-decoration: none;">
-										Authentic Alignment Wellness<br>
-										<a href="mailto:info@authenticalignmentwellness">info@authenticalignmentwellness</a>
-										<br>
-										<a href="https://www.facebook.com/authenticalignmentwellness/">facebook</a>
-										<br>
-										<a href="https://www.instagram.com/authentic.alignment.wellness/">instagram</a>
-									</span>
-								</td>
-							</tr>
-
-						</table>
-					</tr>
-				</tbody>
-			</table>
-		</body>
-	</html>
-				';
-
-				add_filter( 'wp_mail_content_type', 'set_content_type' );
-					function set_content_type( $content_type ) {
-						return 'text/html';
-				}
-
-				wp_mail($to, $subject, $message); // amanda
-				wp_mail($developer, $subject, $message); // developer for testing should be off as a default
-				wp_mail($email, $subject, $message); // client
 				
 				// thanks to the correct person would also be a nice touch
 				wp_redirect( home_url() . '/thanks?n=' . $id );
