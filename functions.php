71e26e7eb92e2b9cec674bd3348f998595fc1c19
Riley Bathurst <riley@rileybathurst.com>
2019-10-18 13:14:25 +1300

    g-

--- functions.php
+++ functions.php
@@ -611,7 +611,7 @@ function prefix_admin_discovery() {
 		// Build POST request:
 		$recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
 		$recaptcha_secret = 'YOUR_RECAPTCHA_SECRET_KEY';
-		$recaptcha_response = $_POST['recaptcha_response'];
+		$recaptcha_response = $_POST['g-recaptcha_response'];
 	
 		// Make and decode POST request:
 		$recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
